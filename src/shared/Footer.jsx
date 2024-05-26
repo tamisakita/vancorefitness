891472151af637a7cb75a22341a9f9c61992deac
Patricia Tami Sakita <tamisakita@hotmail.com>
2024-05-26 12:51:10 -0700

    added footer

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import LogoFooter from "../assets/logo/Logo-Gray.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex items-center justify-center flex-col bg-dark-gray py-10 gap-6">
+      <Link to="/">
+        <img src={LogoFooter} alt="Van Core Fitness Logo" className="h-20" />
+      </Link>
+      <h3 className="font-anton text-2xl text-center text-white">
+        Discover Your Strength.
+        <br />
+        Shape Your Story.
+      </h3>
+    </footer>
+  );
 };
 
 export default Footer;
